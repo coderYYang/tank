5b52d4c39b807749d347512599309a160165964b
yy <691335336@qq.com>
2023-06-22 23:09:44 +0800

    [REM] 删除多余方法

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,11 @@
  * @Author: yy 691335336@qq.com
  * @Date: 2023-06-18 21:02:26
  * @LastEditors: yy 691335336@qq.com
- * @LastEditTime: 2023-06-22 22:23:00
+ * @LastEditTime: 2023-06-22 23:08:59
  * @FilePath: /tank/src/main.ts
  * @Description: 入口文件
  */
 
-import { start, bootstrap } from './app'
+import { bootstrap } from './app'
 
 bootstrap()
-// start()
